97b8210b5eb1c7f84591fdea84bb23dea1d284f2
Oluwaleke Aina <ainaleke@gmail.com>
2016-11-21 11:26:43 -0500

    Created the List view for the Books List, Binding to Adapter coming next
    Created the List view for the Books List, Binding to Adapter coming next

---
+++ app/src/main/java/com/searchbook/BookAdapter.java
@@ -0,0 +1,7 @@
+package com.searchbook;
+
+/**
+ * Created by Olasumbo Ogunyemi on 11/21/2016.
+ */
+    public class BookAdapter {
+}
--- app/src/main/java/com/searchbook/BookListActivity.java
+++ app/src/main/java/com/searchbook/BookListActivity.java
@@ -6,15 +6,27 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+
+import netclient.BookClient;
 
 public class BookListActivity extends AppCompatActivity {
 
+    private ListView bookListView;
+    private BookAdapter bookAdapter;
+    private BookClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
