3a1ec302115bdce56d1a1d594d76bf58b64315f5
Oluwaleke Aina <ainaleke@gmail.com>
2016-12-02 18:38:26 -0500

    Share functionality now implemented. Users can share books with friends

--- app/src/main/java/com/searchbook/DisplayBookActivity.java
+++ app/src/main/java/com/searchbook/DisplayBookActivity.java
@@ -1,14 +1,24 @@
 package com.searchbook;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -17,6 +27,11 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.util.TextUtils;
@@ -97,7 +112,7 @@ public class DisplayBookActivity extends AppCompatActivity {
                         //publishDateStr = ;
                         //Log.i(TAG,"Form: "+ publishDateStr);
 
-                        publishDateView.setText("Published on: "+details.getString("publish_date"));
+                        publishDateView.setText("Published in: "+details.getString("publish_date"));
 
                         if(!details.has("number_of_pages")){
                             pages.setText("Number of Pages: N/A");
@@ -131,5 +146,65 @@ public class DisplayBookActivity extends AppCompatActivity {
             }
         });
     }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_share_book,menu);
+        final MenuItem searchItem=menu.findItem(R.id.menuShare);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        //Handle action bar item clicks here. The action bar will automatically
+        //handle clicks on the Home/Up button, so long as you specify a parent activity in AndroidManifest.xml
+        int id=item.getItemId();
+        if(id==R.id.menuShare){
+            processShareIntent();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    private void processShareIntent() {
+        //grab the image view itself
+        ImageView bookCoverImgView=(ImageView)findViewById(R.id.bookCoverImg);
+        final TextView tvTitle=(TextView)findViewById(R.id.book_details_title);
+        //Get access to the uRI for the image/Bitmap- the image must be converted to bitmap for us to share it
+        Uri bitmapUri=getLocalBitmapUri(bookCoverImgView);
+        //COnstruct a share intent with link to image
+        Intent shareIntent = new Intent();
+        //construct a share Intent with link to image.
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("*/*");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,(String)tvTitle.getText());
+        shareIntent.putExtra(Intent.EXTRA_STREAM,bitmapUri);
+        //Launch the share menu
+        startActivity(Intent.createChooser(shareIntent,"Share Image"));
+    }
+    //returns the URI path to the Bitmap displayed in cover imageView
+    //getLocaBitmapUri() extracts the Bitmap from the IMageView and loads it into a file in the default external storage directory
+    private Uri getLocalBitmapUri(ImageView bookCoverImgView) {
+        //Extract Bitmap from ImageView drawable
+        Drawable drawable = bookCoverImgView.getDrawable();
+        Bitmap bmp=null;
+        if(drawable instanceof BitmapDrawable){
+            bmp=((BitmapDrawable)bookCoverImgView.getDrawable()).getBitmap();
+        }
+        //store image to default external storage directory
+        Uri bmpUri=null;
+        try{
+            //get the location of the pictures directory
+            File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                    "share_image_"+ System.currentTimeMillis()+".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG,90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
 }
